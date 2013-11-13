18f9bdb139b43cc1309be7c2a0d3074b4a3c8cf9
Josh Tanner <mindful.jt@gmail.com>
2013-11-13 12:49:11 -0800

    args

--- kern/test/mytest.c
+++ kern/test/mytest.c
@@ -4,6 +4,10 @@
 
 int mytest(int nargs, char **args){
 	kprintf("My test works!");
+	for (int i = 0; i < nargs; i++){
+		kprintf(args[i]);
+		kprintf("\n");
+	}
 	return 0; //Menu thinks there's an error without a 0 return
 }
 

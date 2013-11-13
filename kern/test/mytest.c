15bf3af50f77671c8a74d2453276923ba14ffee6
Josh Tanner <mindful.jt@gmail.com>
2013-11-13 15:02:20 -0800

    lab 5 finished

--- kern/test/mytest.c
+++ kern/test/mytest.c
@@ -1,6 +1,15 @@
 #include <types.h>
 #include <lib.h>
 #include <test.h>
+#include <thread.h>
+
+void thread_function(void *ptr, unsigned long value){
+	char* name = ptr;
+	//This is just the ugliest thing. Using void values generates compile
+	//errors, but we can use the memory location of a void value and pretend
+	//that it's a char pointer, because we know in this case it will be
+	kprintf(name);
+}
 
 int mytest(int nargs, char **args){
 	kprintf("My test works!");
@@ -8,6 +17,8 @@ int mytest(int nargs, char **args){
 		kprintf(args[i]);
 		kprintf("\n");
 	}
+	//thread_fork can take a null process; it defaults to the current one
+	thread_fork("test thread", NULL, thread_function, "test thread", nargs);
 	return 0; //Menu thinks there's an error without a 0 return
 }
 

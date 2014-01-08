a507d3e1db9c5e426ccaf5c7ed352f66c37575db
Josh Tanner <mindful.jt@gmail.com>
2014-01-07 18:38:32 -0800

    work on threading stuff

--- kern/test/mythreadtest.c
+++ kern/test/mythreadtest.c
@@ -6,9 +6,22 @@
 
 static
 void thread_function(void *ptr, unsigned long value){
-	
+	(void)value;
+	kprintf("Thread go");
+	char* cp = ptr;
+	kprintf((*cp) +'0');	
 }
 
 int mythreadtest(int nargs, char **args){
+	kprintf("Running my thread test");
+	(void)nargs; //Avoid unused parameter warning
+	//args[0] is name of calling program
+	int n = *(args[1])-'0';
+	//The bottom loop doesn't seem to be running, or there's
+	//something wrong with the way we're launching threads
+	for (int i = 0; i < n; ++i){
+		thread_fork("thread test thread", NULL, thread_function, &i, 1);
+	}
 
+	return 0;
 }

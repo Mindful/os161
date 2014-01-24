356495da0e79ee27bd87f0718ae81f49466f02f1
Josh Tanner <mindful.jt@gmail.com>
2014-01-23 16:09:55 -0800

    much better (not perfect) itoa

--- kern/syscall/simple_syscalls.c
+++ kern/syscall/simple_syscalls.c
@@ -17,41 +17,43 @@ void sys__exit(int code){
 
 int sys_printint(int tp){
   int len = 0;
-  int tmp;
   int neg = 0;
   
 
   // kprintf("tp = %d\n", tp);
 
   if (tp < 0) { 
-	  tp = tp - (2 * tp);
+	  tp = -tp;
 	  neg = 1;
+	  ++len;
   }
 
-  for (tmp = tp; tmp > 0; --tmp){
+  //Changed this to divide by 10, because subtraction
+  //just gives us the value, not the length
+  int tmp;
+  for (tmp = tp; tmp > 0; tmp/=10){
     ++len;
   }
-  if (neg)
-	  ++len;
   
   // kprintf("len = %d; attempting kmalloc\n", len);
+ // ++len; //Make room for that null terminating char
   char istr[len];
   
+  if (neg) istr[0]='-';
+
   if (!istr)
 	  panic("memory allocation failed");
 
   // kprintf("attempting sign storage\n");
-  if (neg){
-    istr[tmp] = '-';
-	 ++tmp;
-  }
-
+  tmp = 1;
   // kprintf("constructing string\n");
   while( tp > 0 ){
-   istr[tmp] = '0' + (tp % 10);
+   istr[len-tmp] = '0' + (tp % 10);
 	tp = tp / 10;
 	++tmp;
   }
+  //istr[++len] = 0;
+
  
   // kprintf("printing string\n");
   return kprintf(istr);
